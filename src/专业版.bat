2a840e662eafb316153f556c53f14b8d0aa38fe4
Kai Junru <30892061+caijunru@users.noreply.github.com>
2018-08-19 14:36:22 +0800

    Add files via upload

---
+++ src/专业版.bat
@@ -0,0 +1,5 @@
+slmgr /upk
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms zh.us.to
+slmgr /ato
+slmgr /xpr
---
+++ src/企业版.bat
@@ -0,0 +1,5 @@
+slmgr /upk
+slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+slmgr /skms zh.us.to
+slmgr /ato
+slmgr /xpr
